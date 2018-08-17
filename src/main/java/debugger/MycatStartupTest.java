864a7ea32b78070c81c43b2605b7a7416e6078ca
laowang <wdandyy@163.com>
2018-08-17 15:04:28 +0800

    local debug env

---
+++ src/main/java/debugger/MycatStartupTest.java
@@ -0,0 +1,15 @@
+package debugger;
+
+import io.mycat.MycatStartup;
+
+/**
+ * {@link io.mycat.MycatStartup}测试
+ *
+ */
+public class MycatStartupTest {
+
+    public static void main(String[] args) {
+        MycatStartup.main(args);
+    }
+
+}
